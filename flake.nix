d42aefec80fcbb8b16e3091cfd3e47fb43d20827
hugosenari <hugosenari@gmail.com>
2022-05-09 20:45:06 -0300

    chore: re index uuid

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."uuid-master".dir   = "master";
-  inputs."uuid-master".owner = "nim-nix-pkgs";
-  inputs."uuid-master".ref   = "master";
-  inputs."uuid-master".repo  = "uuid";
-  inputs."uuid-master".type  = "github";
-  inputs."uuid-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."uuid-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
